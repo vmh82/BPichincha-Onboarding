f682c561c67312a9f846caa395e6cdeb381758df
Victor Maldonado <victor_fab92@hotmail.com>
2022-08-08 10:47:24 -0500

    Se elimina namespace no utilizados

--- BPichincha.CreditoAuto/test/CreditoAuto.Test/IntegrationTest/ClienteServiceTest.cs
+++ BPichincha.CreditoAuto/test/CreditoAuto.Test/IntegrationTest/ClienteServiceTest.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
